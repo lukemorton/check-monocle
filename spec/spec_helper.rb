cf6d360ca5c53ec2389b021f826da256449ad93a
Luke Morton <lukemorton.dev@gmail.com>
2017-11-11 22:38:27 +0000

    Remove timeout (defaults to 30)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,7 +17,6 @@ Capybara.javascript_driver = :poltergeist
 Capybara.register_driver :poltergeist do |app|
   Capybara::Poltergeist::Driver.new(
     app,
-    timeout: 10,
     phantomjs_logger: '/dev/null',
     js_errors: false
   )
