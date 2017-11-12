cfeadae68af6bea53c2d30cefa7aca236c7cb6e5
Luke Morton <lukemorton.dev@gmail.com>
2017-11-12 00:02:31 +0000

    Set default descriptions for all shows

--- spec/radio_shows_spec.rb
+++ spec/radio_shows_spec.rb
@@ -31,42 +31,49 @@ describe 'Radio Shows' do
 
   context 'Culture with Robert Bound', monday: true do
     let(:show) { 'culture-with-robert-bound' }
+    let(:default_description) { 'Our Culture editor and his guests discuss' }
     include_examples 'latest show'
     include_examples 'soon to be released show'
   end
 
   context 'Section D', tuesday: true do
     let(:show) { 'section-d' }
+    let(:default_description) { 'Everything you need to know about the world of' }
     include_examples 'latest show'
     include_examples 'soon to be released show'
   end
 
   context 'The Entrepreneurs', wednesday: true do
     let(:show) { 'the-entrepreneurs' }
+    let(:default_description) { 'Monocle 24’s weekly tour of the most inspiring' }
     include_examples 'latest show'
     include_examples 'soon to be released show'
   end
 
   context 'The Urbanist', thursday: true do
     let(:show) { 'the-urbanist' }
+    let(:default_description) { 'With an influential audience of city mayors' }
     include_examples 'latest show'
     include_examples 'soon to be released show'
   end
 
   context 'The Sessions', friday: true do
     let(:show) { 'the-sessions-at-midori-house' }
+    let(:default_description) { 'Our standalone home of live music presented' }
     include_examples 'latest show'
     include_examples 'soon to be released show'
   end
 
   context 'The Menu', friday: true do
     let(:show) { 'the-menu' }
+    let(:default_description) { 'Our guide to the world of food, drink' }
     include_examples 'latest show'
     include_examples 'soon to be released show'
   end
 
   context 'The Curator', friday: true do
     let(:show) { 'the-curator' }
+    let(:default_description) { 'The best of the week on Monocle 24' }
 
     example 'Latest show is valid' do
       visit radio_show_url(show)
@@ -86,30 +93,35 @@ describe 'Radio Shows' do
 
   context 'The Stack', friday: true do
     let(:show) { 'the-stack' }
+    let(:default_description) { 'Essential listening for anyone who cares about' }
     include_examples 'latest show'
     include_examples 'soon to be released show'
   end
 
   context 'The Foreign Desk', friday: true do
     let(:show) { 'the-foreign-desk' }
+    let(:default_description) { 'flagship global-affairs show featuring interviews' }
     include_examples 'latest show'
     include_examples 'soon to be released show'
   end
 
   context 'Meet the Writers', friday: true do
     let(:show) { 'meet-the-writers' }
+    let(:default_description) { 'Want to know more about the authors behind' }
     include_examples 'latest show'
     include_examples 'soon to be released show'
   end
 
   context 'The Monocle Weekly', saturday: true do
     let(:show) { 'the-monocle-weekly' }
+    let(:default_description) { 'Want to hear from the authors, artists, creative' }
     include_examples 'latest show'
     include_examples 'soon to be released show'
   end
 
   context 'The Bulletin with UBS', saturday: true do
     let(:show) { 'the-bulletin-with-ubs' }
+    let(:default_description) { 'Delivering insights into the people, places' }
     include_examples 'latest show'
     include_examples 'soon to be released show'
   end
@@ -132,7 +144,7 @@ describe 'Radio Shows' do
   def next_show_url
     show_number = latest_show_link['href'].scan(/\/(\d+)\//)[0][0]
     next_show_number = show_number.to_i.next.to_s
-    next_show_url = latest_show_link['href'].sub(show_number, next_show_number)
+    latest_show_link['href'].sub(show_number, next_show_number)
   end
 
   # We assert the the <h1> content is found and used in <title>
@@ -144,12 +156,12 @@ describe 'Radio Shows' do
 
   def assert_page_has_hero
     hero_style_attr = page.find('.radio-hero')['style']
-    expect(hero_style_attr).to_not include('generic-episode-photo')
+      expect(hero_style_attr).to_not include('generic-episode-photo')
     expect(hero_style_attr).to match(/jpg|jpeg/)
   end
 
   def assert_page_has_description
-    expect(page.first('.meta-summary').text).to_not eq('With an influential audience of city mayors')
+    expect(page.first('.meta-summary').text).to_not eq(default_description)
   end
 
   def assert_if_page_has_chapters_that_they_are_valid
